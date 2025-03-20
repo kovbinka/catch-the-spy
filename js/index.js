ed7b9db349517a057cb101d3ea2f2961f061ab3f
kovbinka <roman10kovbinka@gmail.com>
2025-03-20 11:36:35 +0100

    fixed sound fx

--- js/index.js
+++ js/index.js
@@ -3,21 +3,20 @@ let currCivilianTile;
 let score = 0;
 let gameOver = false;
 let spyClicked = false;
+const backgroundSound = document.getElementById("background");
+const catchSound = document.getElementById("catch");
+const endgameSound = document.getElementById("endgame");
 
 window.onload = function() {
   setGame();
 }
 
-let backgroundSound = getElementById("background");
-let catchSound = getElementById("catch");
-let endgameSound = getElementById("endgame");
-
 function setGame() {
   for (let i = 0; i < 9; i++) {
     let tile = document.createElement("div");
     tile.id = i.toString(); // <= id must be a string (but js will convert this automatically anyway)
-    tile.addEventListener("click", selectTile);
     document.getElementById("board").appendChild(tile);
+    tile.addEventListener("click", selectTile);
   }
 
   setInterval(setSpy, 1000);
@@ -89,12 +88,14 @@ function selectTile() {
     score += 10;
     document.getElementById("span").innerText = score.toString();
     spyClicked = true;
+    catchSound.play();
+    catchSound.volume = 0.5;
   }
 
   else if (this == currCivilianTile) {
-
     document.getElementById("score").innerText = "GAME OVER, YOUR SCORE IS: \n" + score.toString();
     gameOver = true;
     endgameSound.play();
+    endgameSound.volume = 0.5;
   }
 }
